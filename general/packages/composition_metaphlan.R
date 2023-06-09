7f16ebb0fe1394c77e4008c5f8a9d2e3efa3b1cc
wanjin.hu <wanjin.hu@outlook.com>
2023-06-09 19:58:50 +0800

    update_20230609

--- general/packages/composition_metaphlan.R
+++ general/packages/composition_metaphlan.R
@@ -1,3 +1,13 @@
+#!/usr/bin/Rscript
+# -*- encoding: utf-8 -*-
+
+#@File    :   composition_metaphlan.R
+#@Time    :   2023/06/09 14:30:50
+#@Author  :   Wanjin.Hu 
+#@Version :   1.0
+#@Contact :   wanjin.hu@diprobio.com
+#@Description :
+
 composition <- function(tax_lev) {
   "
   @功能: 对格式为<06_metaphlan_species.txt>的物种表做柱状组成图
@@ -11,13 +21,13 @@ composition <- function(tax_lev) {
   spe <- df_merge[-c(1:7)]
   tax <- df_merge[1:7]
   phy <- spe %>%
-    group_by(tax$`tax_lev`) %>% # 使用tax中的X水平进行分类
+    group_by(tax[[tax_lev]]) %>% # 使用tax中的X水平进行分类
     summarise_all(sum) %>%
-    rename(`tax_lev` = `tax$tax_lev`) %>%
-    gather(key="samples",value = "abun",-`tax_lev`) %>% # 数据形式转换：“宽”转“长”
+    rename_at(1, ~ tax_lev) %>%
+    gather(key="samples",value = "abun",- tax_lev) %>% # 数据形式转换：“宽”转“长”
     left_join(group,by=c("samples"="sample"))
   p1 <-
-    ggplot(phy,aes(samples,abun,fill=`tax_lev`)) + 
+    ggplot(phy,aes(samples,abun,fill=.data[[tax_lev]])) + 
     geom_bar(stat = "identity",position = "fill") +
     facet_grid(. ~ group, scales = "free", space = "free_x") +
     labs(x="",y="Percent")+
@@ -29,21 +39,61 @@ composition <- function(tax_lev) {
   return(p1)
 }
 
+library(getopt)
+command <- matrix(c(
+  "help", "h", 0, "logical", "Help docs",
+  "data", "d", 1, "character", "data file",
+  "group", "g", 1, "character", "group file",
+  "tax", "t", 1, "character", "which lev you graph"),
+  byrow = TRUE, ncol = 5
+)
+args <- getopt(command)
+if (! is.null(args$help) || 
+    is.null(args$data) || 
+    is.null(args$group) ||
+    is.null(args$tax)) {
+  cat(paste(getopt(command, usage = TRUE), "\n"))
+  q(status = 1)
+}
+
 library(tidyverse)
 library(ggsci)
 library(ggplot2)
-df <- read.table("06_metaphlan_species.txt",
+df <- read.table(args$data,
                  header = T,
                  sep = "\t",
                  check.names = F)
-group <- read.table("group.txt",
+group <- read.table(args$group,
                     header = T,
                     sep = "\t",
                     check.names = F)
-col=pal_d3("category20")(20)
-col2 = pal_d3("category20",alpha = 0.5)(20)
-mypal=c(col,col2)
-p1 <- composition(tax_lev = "phylum")
-pdf("phylum_percent.pdf",width = 12,height = 10,family="Times")
+tax <- args$tax
+# 颜色设置
+colors_lancet <- pal_lancet("lanonc",alpha = 1)(9)
+colors_npg <- pal_npg("nrc",alpha = 1)(10)
+colors_d3_20 <- pal_d3("category20",alpha = 1)(20)
+colors_igv <- pal_igv("default",alpha = 1)(51)
+mypal=c(colors_lancet,colors_npg,colors_d3_20,colors_igv)
+p1 <- composition(tax_lev = tax)
+pdf(paste0(tax,"_percent.pdf"),width = 12,height = 10,family="Times")
 p1
 dev.off()
+message("Finish composition plot!")
+
+## 本地测试
+# df <- read.table("06_metaphlan_species.txt",
+#                  header = T,
+#                  sep = "\t",
+#                  check.names = F)
+# group <- read.table("group.txt",
+#                     header = T,
+#                     sep = "\t",
+#                     check.names = F)
+# col=pal_d3("category20")(20)
+# col2 = pal_d3("category20",alpha = 0.5)(20)
+# mypal=c(col,col2)
+# p1 <- composition(tax_lev = "phylum")
+
+# pdf("phylum_percent.pdf",width = 12,height = 10,family="Times")
+# p1
+# dev.off()
